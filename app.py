22995dee7ce1e5f4547ca0dd12b00084d3a34d76
Lukas Marschall <lmarschall.com@gmail.com>
2020-08-27 20:07:11 +0200

    added static files

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
-@app.route("/cpu_count")
-def me_api():
+@app.route("/api")
+def api():
     cpu_count = psutil.cpu_count()
     return {
         "username": cpu_count
