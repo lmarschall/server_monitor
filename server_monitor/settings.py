66ce2e8df8e86b81f8d24ae9c1b5257b53f999b9
Lukas Marschall <lmarschall.com@gmail.com>
2020-08-27 13:16:07 +0200

    added server ip to allowed hosts

--- server_monitor/settings.py
+++ server_monitor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+u@d9+60az0qpp4&fxtw-e#k!po&4-rcxc_sp&=9hxl&uj&*o^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.122']
 
 
 # Application definition
