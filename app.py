e7fba8bf448c58e9d53736026e5148beae94b38a
Lukas Marschall <lmarschall.com@gmail.com>
2020-09-11 09:21:13 +0200

    created returning metrcis dictionary

--- app.py
+++ app.py
@@ -9,7 +9,25 @@ def index():
 
 @app.route("/api")
 def api():
-    cpu_count = psutil.cpu_count()
+
+    # define dictionary for saving metrics of server
+    metrics = {}
+
+    # get the number of cpus
+    metrics.update(cpu_count=psutil.cpu_count())
+
+    # get the system wide cpu utilization
+    metrics.update(cpu_utilization=psutil.cpu_percent())
+
+    # get the utilization for each cpu
+    metrics.update(cpu_utilization_per_cpu=psutil.cpu_percent(percpu=True))
+
+    # get the cpu frequences
+    psutil.cpu_freq(percpu=False)
+
+    # get the memory of the server
+    metrics.update(memory=psutil.virtual_memory())
+
     return {
-        "username": cpu_count
+        "metrics": metrics
     }
